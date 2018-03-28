53b89edef55f638f4e11b65742b939622c9860aa
ooge0 <dns.rom@gmail.com>
2018-03-28 15:27:22 +0300

    1

--- src/test/java/Steps/LoginStep.java
+++ src/test/java/Steps/LoginStep.java
@@ -1,8 +1,8 @@
 package Steps;
 
 import cucumber.api.PendingException;
-import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 
 public class LoginStep {
