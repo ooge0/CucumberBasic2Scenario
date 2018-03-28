193c807839bfb0fe7387cca0d381708271d1c550
ooge0 <dns.rom@gmail.com>
2018-03-28 18:55:43 +0300

    Implementation of POJO (Plain Old Java Object ) approach

--- src/test/java/Features/Login.feature
+++ src/test/java/Features/Login.feature
@@ -3,7 +3,7 @@ Feature: LoginFeature
   Scenario: Login with correct user name and password
     Given I navigate to the login page
     And I enter the the following for login
-      | UserName | Password      |
+      | username | password      |
       | admin    | adminpassword |
     And I click the login button
     Then I should see the userform
--- src/test/java/Steps/LoginStep.java
+++ src/test/java/Steps/LoginStep.java
@@ -1,10 +1,13 @@
 package Steps;
 
+import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
 import cucumber.api.DataTable;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 
+import java.util.AbstractList;
+import java.util.ArrayList;
 import java.util.List;
 
 // change order of method 1
@@ -13,15 +16,16 @@ public class LoginStep {
     @Given("^I navigate to the login page$")
     public void iNavigateToLoginPage() throws Throwable {
         System.out.println("I navigate to the login page");
-    //throw new PendingException();
+        //throw new PendingException();
 
     }
+
     @And("^I click the login button$")
     public void iClickLoginButton() throws Throwable {
         System.out.println("I click the login form");
         // throw new PendingException();
     }
-    
+
     @Then("^I should see the userform$")
     public void iShouldSeeTheUserform() throws Throwable {
         System.out.println("I should see the user form");
@@ -31,8 +35,34 @@ public class LoginStep {
     @And("^I enter the the following for login$")
     public void iEnterTheTheFollowingForLogin(DataTable table) throws Throwable {
         List<List<String>> data = table.raw();
-        System.out.println("This value is " + data.get(0).get(0).toString());
-        System.out.println("This value is "+ data.get(0).get(1).toString());
+       
+        //create an ArrayList
+        List<User> users = new ArrayList<User>();
+
+        // store all users
+        users = table.asList(User.class);
+        //create FOR cycle for each elements of List <user>
+
+        for (User user: users) {
+            System.out.println("UserName is " + user.username);
+            System.out.println("Password is " + user.password);
+        }
+
         //throw new PendingException();
     }
+
+    public class User {
+
+        public String username;
+        public String password;
+
+        public User(String userName, String passWord) {
+            username = userName;
+            password = passWord;
+        }
+
+
+
+    }
+
 }
