30194fc3d736e79e95e0b891b22642fd085dfc1f
ooge0 <dns.rom@gmail.com>
2018-03-28 16:09:59 +0300

    "modify feature file to. apply DataTable design"

--- src/test/java/Features/Login.feature
+++ src/test/java/Features/Login.feature
@@ -2,6 +2,8 @@ Feature: LoginFeature
 
   Scenario: Login with correct user name and password
     Given I navigate to the login page
-    And I enter the username as "admin" and password as "adminPassword"
+    And I enter the the following for login
+      | UserName | Password      |
+      | admin    | adminpassword |
     And I click the login button
     Then I should see the userform
--- src/test/java/Steps/LoginStep.java
+++ src/test/java/Steps/LoginStep.java
@@ -1,10 +1,13 @@
 package Steps;
 
-import cucumber.api.PendingException;
+import cucumber.api.DataTable;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
-// change order of methods
+
+import java.util.List;
+
+// change order of method 1
 public class LoginStep {
 
     @Given("^I navigate to the login page$")
@@ -18,16 +21,18 @@ public class LoginStep {
         System.out.println("I click the login form");
         // throw new PendingException();
     }
-
-
-    @And("^I enter the username as \"([^\"]*)\" and password as \"([^\"]*)\"$")
-    public void iEnterTheUserNameAsAndPasswordAs(String username, String password) throws Throwable {
-        System.out.println("Username is " + username + " and password is "+ password);
-        //throw new PendingException();
-    }
+    
     @Then("^I should see the userform$")
     public void iShouldSeeTheUserform() throws Throwable {
         System.out.println("I should see the user form");
         // throw new PendingException();
+    }
+
+    @And("^I enter the the following for login$")
+    public void iEnterTheTheFollowingForLogin(DataTable table) throws Throwable {
+        List<List<String>> data = table.raw();
+        System.out.println("This value is " + data.get(0).get(0).toString());
+        System.out.println("This value is "+ data.get(0).get(1).toString());
+        //throw new PendingException();
     }
 }
